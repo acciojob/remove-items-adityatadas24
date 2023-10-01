6439ae80925ffd7ebbdf31c50b8f08da2337c068
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-10-01 11:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    
+      var colorSelects = document.getElementById("colorSelect");
+      
+    
+      var selectedIndex = colorSelects.selectedIndex;
+      
+     
+      if (selectedIndex !== -1) {
+
+        colorSelects.remove(selectedIndex);
+      }
+    }
